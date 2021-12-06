3cad3152ef0b04b05d70ff4c8a419455fc36f298
Vaclav Voracek <vaclav.voracek@uni-tuebingen.de>
2021-12-06 11:59:35 +0100

    works but slowly

--- main.py
+++ main.py
@@ -1,44 +1,57 @@
 import jax.numpy as np
-from jax import jit, vmap
-import jax 
-from jax.lax import scan, cond, fori_loop, dynamic_slice
-from jax.util import partial, safe_zip
+from jax import jit
+from jax.lax import cond, fori_loop
+from jax.util import partial
 import pickle 
 from time import time 
 import numpy as onp
-from torch.utils import data
-from torchvision.datasets import MNIST
+from matplotlib import pyplot as plt 
+import torch 
+from torchvision.transforms import functional as tvF
+import scipy.io
 
-def numpy_collate(batch):
-  if isinstance(batch[0], onp.ndarray):
-    return onp.stack(batch)
-  elif isinstance(batch[0], (tuple,list)):
-    transposed = zip(*batch)
-    return [numpy_collate(samples) for samples in transposed]
-  else:
-    return onp.array(batch)
 
-class NumpyLoader(data.DataLoader):
-  def __init__(self, dataset, batch_size=1,
-                shuffle=False, sampler=None,
-                batch_sampler=None, num_workers=0,
-                pin_memory=False, drop_last=False,
-                timeout=0, worker_init_fn=None):
-    super(self.__class__, self).__init__(dataset,
-        batch_size=batch_size,
-        shuffle=shuffle,
-        sampler=sampler,
-        batch_sampler=batch_sampler,
-        num_workers=num_workers,
-        collate_fn=numpy_collate,
-        pin_memory=pin_memory,
-        drop_last=drop_last,
-        timeout=timeout,
-        worker_init_fn=worker_init_fn)
+class NumpyLoader():
+    def __init__(self, X, Y, batch_size=1, shift = None):
+        if(shift):
+            if(X.shape[-1] != 28**2):
+                raise NotImplementedError
 
-class FlattenAndCast(object):
-  def __call__(self, pic):
-    return onp.ravel(onp.array(pic, dtype=np.float32))
+            X = torch.tensor(X)
+            _X = onp.zeros((X.shape[0], (2*shift+1)**2, X.shape[1]))
+            idx =  0
+            X = X.reshape(-1,28,28)
+
+            for Tx in range(-shift,1+shift):
+                for Ty in range(-shift,1+shift):
+                    _X[:, idx] = onp.array(tvF.crop(X, Tx, Ty, 28,28).reshape(X.shape[0], -1))
+                    idx += 1
+            X = _X
+        else:
+            X = X[:,None,:]
+
+        self.X = X
+        self.Y = Y
+        self.batch_size = batch_size 
+        self.curr_idx = 0
+        self.inds = onp.arange(X.shape[0])
+        onp.random.shuffle(self.inds)
+
+    def __iter__(self):
+        return self 
+
+    def __next__(self):
+        if(self.curr_idx*self.batch_size >= self.X.shape[0]):
+            self.curr_idx = 0
+            onp.random.shuffle(self.inds)
+            raise StopIteration
+        else:
+            augment = onp.random.randint(self.X.shape[1], size=self.batch_size)
+            inds = self.inds[self.curr_idx * self.batch_size : (1+self.curr_idx) * self.batch_size]
+            self.curr_idx += 1
+            return np.array(self.X[inds, augment]), np.array(self.Y[inds])
+    def size(self):
+        return self.X.shape[0]
 
 @jit
 def _l1(a,b):
@@ -74,44 +87,45 @@ def pairwise_distances(Xs, Ys, pnorm):
     elif(pnorm == 2):
         XX = (Xs**2).sum(-1).reshape(-1,1) 
         YY = (Ys**2).sum(-1).reshape(1,-1)
-
         return np.abs(XX + YY - 2* Xs @ Ys.T )**0.5
 
 def _else_branch(vals):
-    best_i, best_j, best_lb, lbs, idx, WX, Wdist,y, curr_i = vals
+    best_i, best_j, best_lb, lbs, idx, WX, Wdist,y, curr_i, masks = vals
     dists = (WX[:,idx] - WX[curr_i, idx]) / Wdist[curr_i]
     dists = np.where(masks[y,1], dists, np.inf)
     j = np.argmin(dists)
     return cond(dists[j] < best_lb,
-            lambda _: (best_i, best_j, best_lb, lbs, idx, WX, Wdist,y),
-            lambda _: (curr_i, j, dists[j], lbs, idx, WX, Wdist, y),
+            lambda _: (best_i, best_j, best_lb, lbs, idx, WX, Wdist,y,masks),
+            lambda _: (curr_i, j, dists[j], lbs, idx, WX, Wdist, y, masks),
             None 
     )
 
 def _forifun2(curr_i, carry):
-    best_i, best_j, best_lb, lbs, idx, WX, Wdist,y = carry
-
-
+    best_i, best_j, best_lb, lbs, idx, WX, Wdist,y, masks = carry
+    
     return cond(lbs[curr_i] < best_lb, 
-            lambda _: (best_i, best_j, best_lb, lbs, idx, WX, Wdist, y),
+            lambda _: (best_i, best_j, best_lb, lbs, idx, WX, Wdist, y, masks),
             _else_branch,
-            (best_i, best_j, best_lb, lbs, idx, WX, Wdist,y, curr_i)
+            (best_i, best_j, best_lb, lbs, idx, WX, Wdist,y, curr_i, masks)
         )    
 
 def _forifun(idx, val):
-    lbs_idx, ret, retidx, mindistj, minWdist, WX, Wdist, start, ppc,y = val 
-    lbs2 = (mindistj[idx] - WX[:, idx])/minWdist
+    lbs_idx, ret, retidx, mindistj, minWdist, maxWdist, WX, Wdist, start, ppc,y,masks = val 
+    lbs2  = (mindistj[idx] - WX[:, idx])/minWdist
+    lbs2_ = (mindistj[idx] - WX[:, idx])/maxWdist 
+    lbs2 = np.maximum(lbs2, lbs2_)
 
-    best_i, best_j, best_lb, lbs, idx, _, _, _ = fori_loop( start, start+ppc, _forifun2,  (-1, -1, -1000000.0, lbs2, idx, WX, Wdist,y))
+    best_i, best_j, best_lb, lbs, idx, *_ = fori_loop( start, start+ppc, _forifun2,  (-1, -1, -np.inf, lbs2, idx, WX, Wdist,y, masks))
     ret = ret.at[retidx, :6].set((best_i, best_j, WX[best_i, idx], WX[best_j, idx], Wdist[best_i, best_j], best_lb))
-    return lbs_idx, ret, retidx+1, mindistj, minWdist, WX, Wdist, start, ppc,y
+    return lbs_idx, ret, retidx+1, mindistj, minWdist, maxWdist, WX, Wdist, start, ppc,y, masks
 
-def lossfn(X,Y,W,pnorm,masks,ppc,num_classes):
+def lb_fn(X,Y,W,pnorm,masks,ppc,num_classes):
     WW = (W*W).sum(-1)
     XX = (X*X).sum(-1)
     _WX = WW.reshape(-1,1) - 2*W @ X.T + XX.reshape(1,-1)
     Wdist = 2*pairwise_distances(W, W, pnorm)
     minWdist = np.min(Wdist, axis=0)
+    maxWdist = np.max(Wdist, axis=0)
     retidx = 0
     ret = np.zeros((X.shape[0],6 + X.shape[1]))
     for y in range(num_classes):
@@ -119,65 +133,111 @@ def lossfn(X,Y,W,pnorm,masks,ppc,num_classes):
         if(xs.shape[0] == 0):
             continue
         WX = _WX[:, Y==y]
+        rng = np.arange(xs.shape[0])
         start = y*ppc 
-
         arr1 = WX[masks[y,0]]
         i  = np.argmin(arr1, axis=0)
-        mindisti = arr1[i]
+        mindisti = arr1[i,rng]
 
         arr2 =  WX[masks[y,1]]
         j = np.argmin(arr2, axis=0)
-        mindistj = np.min(arr2, axis=0)
+        mindistj = arr2[j, rng]
         i = i+start 
         j = np.where(j < start, j, j+ppc)
 
         ret = ret.at[retidx:retidx+xs.shape[0], 6:].set(xs)
 
         lbs_idxs = (mindistj - mindisti)/Wdist[i,j]
-        _, ret, retidx, *_ = fori_loop(0, xs.shape[0], _forifun, (lbs_idxs, ret, retidx, mindistj, minWdist, WX, Wdist, start, ppc, y))
+        _, ret, retidx, *_ = fori_loop(0, xs.shape[0], _forifun, (lbs_idxs, ret, retidx, mindistj, minWdist, maxWdist, WX, Wdist, start, ppc, y, masks))
     return ret 
 
-def load():
-    with open('mnist.pkl', 'rb') as f:
-        ds = pickle.load(f)
+def load(dataset):
+    if(dataset == 'mnist'):
+        with open('mnist.pkl', 'rb') as f:
+            ds = pickle.load(f)
 
-    Xtrn = ds['training_images']/255.0
-    Ytrn = ds['training_labels']
-    Xtst = ds['test_images']/255.0
-    Ytst = ds['test_labels']
+        Xtrn = ds['training_images']/255.0
+        Ytrn = ds['training_labels']
+        Xtst = ds['test_images']/255.0
+        Ytst = ds['test_labels']
 
-    return (Xtrn, Ytrn), (Xtst, Ytst)
+        return (Xtrn, Ytrn), (Xtst, Ytst)
 
-def train(trngen, tst, W, epochs, pnorm, masks, ppc, num_classes, train_eps, test_eps):
-    for e in range(epochs):
-        acc = 0
+def plot_w(W, ppc, num_classes):
+    if(W.shape[-1] == 28*28): # mnist
+        fig, ax = plt.subplots(nrows=num_classes, ncols=10, sharex=True, sharey=True,)
+        for idx in range(num_classes):    
+            for jdx in range(10): 
+                ax[idx,jdx].imshow(W[ppc*idx+jdx, :].reshape(28,28), cmap='gray')
+        plt.show()
+    else:
+        raise NotImplementedError
+        
+def grad_lb(W,X,I,J, dIJ,lbs,train_eps):
+    gradI = -(W[I]-X)/dIJ - lbs * (W[I] - W[J])/dIJ**2
+    gradJ =  (W[J]-X)/dIJ + lbs * (W[I] - W[J])/dIJ**2
+
+    gradI = np.where(lbs.T < train_eps, gradI.T, 0).T 
+    gradJ = np.where(lbs.T < train_eps, gradJ.T, 0).T 
+
+    gradW = np.zeros_like(W)
+    gradW = gradW.at[I].add(gradI)
+    gradW = gradW.at[J].add(gradJ)
+    return gradW
+
+
+def grad_glvq(W,X,I,J,lbs,train_eps, pnorm):     
+    shift = (W[J] - W[I])
+    norms = ((shift**2).sum(-1)**0.5)
+    AEs = X + train_eps * ( ((W[J] - W[I]).T)/norms).T
+    WIX = W[I] - AEs 
+    WJX = W[J] - AEs
+
+    dix = np.sum(WIX**2, -1)**0.5 
+    djx = np.sum(WJX**2, -1)**0.5 
+    dixdjx2 = (dix+djx)**2
+
+    gradI = (-2 * WIX.T * djx / (dix*dixdjx2)).T
+    gradJ = ( 2 * WJX.T * dix / (djx*dixdjx2)).T
+
+    #gradI = np.where(lbs.T < train_eps, gradI.T, 0).T 
+    #gradJ = np.where(lbs.T < train_eps, gradJ.T, 0).T 
+
+    gradW = np.zeros_like(W)
+    gradW = gradW.at[I].add(gradI)
+    gradW = gradW.at[J].add(gradJ)
+    return gradW
+
+
+
+def train(loader, tst, W, epochs, pnorm, masks, ppc, num_classes, train_eps, test_eps, lr):
+    for e in range(1,1+epochs):
+        if( e%4 == 0):
+            lr *= 0.7
         start = time()
-        for X, Y in trngen:
-            
+        for bidx, (X, Y) in enumerate(loader):
 
-            loss = lossfn(X,Y,W,pnorm,masks,ppc,num_classes)
-            I, J, dIX, dJX, dIJ, lbs =  (loss.T)[:6]
+            lowebounds = lb_fn(X,Y,W,pnorm,masks,ppc,num_classes)
+            I, J, dIX2, dJX2, dIJ, lbs =  (lowebounds.T)[:6]
+
             I = I.astype(np.int32); J =J.astype(np.int32)
-            dIJ = dIJ[:, None]; lbs = lbs[:,None]
-            X = loss[:,6:]
+            dIJ = dIJ[:, None]/2; lbs = lbs[:,None]; X = lowebounds[:,6:]
 
-            acc += sum(lbs > -test_eps)
+            #gradW = grad_lb(W, X, I, J, dIJ, lbs, train_eps)
+            gradW = grad_glvq(W,X,I,J,lbs,train_eps, pnorm)
+            correctly_classified = np.sum(lbs[:,0] > test_eps)
 
 
-            gradI = -(W[I]-X)/dIJ - (lbs * W[I] - W[J])/dIJ**2
-            gradJ =  (W[J]-X)/dIJ + (lbs * W[I] - W[J])/dIJ**2
+            W = W + gradW/np.sum(gradW*gradW)*lr/10
+            #W = W + gradW*lr/X.shape[0]
 
-            gradI = np.where(lbs.T > -train_eps, gradI.T, 0).T 
-            gradJ = np.where(lbs.T > -train_eps, gradJ.T, 0).T 
-            grad = np.zeros_like(W)
-            grad = grad.at[I].add(gradI)
-            grad = grad.at[J].add(gradJ)
-            W = W + grad
+            print(f'epoch: {e:3d}, batch: {bidx:3d}, batch accuracy: {correctly_classified/X.shape[0]:.4f}, batch_loss: {np.sum(np.maximum(train_eps-lbs, 0)):.2f}')
+
         X, Y = tst 
-        print(lbs)
-        _, _, _, _, _,lbs, *_ = lossfn(X,Y,W,pnorm,masks,ppc,num_classes)
-        print(f'epoch: {e}, trn acc: {acc/60000}, tst_acc {np.sum(lbs > -test_eps)/10000}, epoch took {time()-start} sec')
-
+        lowebounds = lb_fn(X,Y,W,pnorm,masks,ppc,num_classes)
+        lbs =  (lowebounds.T)[5]
+        print(f'epoch: {e}, tst_acc {(np.sum(lbs > test_eps)/X.shape[0]):.4f}, epoch took {time()-start:.2f} sec')
+    return W
 
 def get_w(X, Y, ppc, num_classes):
     dim = X.shape[-1]
@@ -195,41 +255,25 @@ def get_masks( ppc, num_classes):
         start =  i    * ppc 
         end   = (i+1) * ppc
             
-        rng = np.arange(W.shape[0])
+        rng = np.arange(ppc*num_classes)
         mask1 = (rng >= start) & (rng < end)
         mask2 = ~mask1
         masks.append((mask1, mask2))
     return np.array(masks)    
 
 if(__name__ == '__main__'):
-    import numpy as onp
     ppc, num_classes = 100,10
 
-    trn, tst = load()
+    trn, tst = load('mnist')
     W = get_w(*trn, ppc, num_classes)
+
+    #db = scipy.io.loadmat('MNIST-TrainTestNonBinary.mat')
+    #trn, tst = (db['Xtrain'], db['Ytrain'].reshape(-1)), (db['Xtest'], db['Ytest'].reshape(-1))
+
     masks = get_masks(ppc, num_classes)
 
-    mnist_dataset = MNIST('/tmp/mnist/', download=True, transform=FlattenAndCast())
-    training_generator = NumpyLoader(mnist_dataset, batch_size=5000, num_workers=0)
+
+    loader = NumpyLoader(*trn, batch_size = 5000, shift=0)
     
-    train(training_generator, tst, W, epochs = 100, pnorm = 2, masks=masks, ppc=ppc, num_classes=num_classes,
-            train_eps = 2.5, test_eps = 1.58)
-
-    ppc, num_classes, d, b = 100,9,3*32**2, 2**12
-
-    W = np.array(onp.random.rand(ppc*num_classes, d))
-    Xs = np.array(onp.random.rand(b, d))
-    Ys = np.array(onp.random.randint(0, num_classes, (b,)))
-
-
-
-    for ii in 2**np.arange(5,13):
-        for _ in range(3):
-            start = time()
-
-            I, J, dI, dJ, dIJ, lbs = lossfn_new(Xs[:ii],Ys[:ii],W,2,masks,ppc,num_classes).T
-            grad = np.zeros_like(W)
-
-            print(time()-start)
-        print()
-
+    W = train(loader, tst, W, epochs = 100, pnorm = 2, masks=masks, ppc=ppc, num_classes=num_classes, train_eps = 2.5, test_eps = 1.58, lr=2000)
+    plot_w(W, ppc, num_classes)
